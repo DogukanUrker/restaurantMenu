3c53df728382d3f43710b8e4581cd82ba3db35bc
Doğukan Ürker <dogukanurker@icloud.com>
2023-06-17 14:12:09 +0300

    fetching menu

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask,render_template,redirect
-
+from json import load
 
 app = Flask(__name__)
 
+
+
+def fetchMenu():
+    products = load(open("products.json"))
+    return products["foods"],products["drinks"],products["desserts"]
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",menu = fetchMenu())
 
 
 if __name__ == "__main__":
