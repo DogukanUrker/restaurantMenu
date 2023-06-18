560ea02e52951ec014d4477587bcae04ecdbb116
Doğukan Ürker <dogukanurker@icloud.com>
2023-06-18 12:33:53 +0300

    /login added

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request, redirect, session
 from json import load
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(32)
 
 
 @app.route("/")
@@ -12,6 +14,17 @@ def index():
         drinks=load(open("products/drinks.json")),
         desserts=load(open("products/desserts.json")),
     )
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if request.form["username"] == "admin" and request.form["password"] == open(
+            "adminPassword.txt", "r"
+        ).read().strip("\n"):
+            session["admin"] = True
+            return redirect("/admin")
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
