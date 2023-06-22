9cba2a7cadff02b85280b9b681e6c8fc93b6090a
Doğukan Ürker <dogukanurker@icloud.com>
2023-06-22 21:22:02 +0300

    simplified

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, request, redirect, session
-import secrets
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.secret_key = secrets.token_urlsafe(32)
 
 
 def loadData():
@@ -23,24 +21,6 @@ def index():
     return render_template(
         "index.html", foods=loadData()[0], drinks=loadData()[1], desserts=loadData()[2]
     )
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        if request.form["username"] == "admin" and request.form["password"] == open(
-            "adminPassword.txt", "r"
-        ).read().strip("\n"):
-            session["admin"] = True
-            return redirect("/admin")
-    return render_template("login.html")
-
-
-@app.route("/admin")
-def admin():
-    if "admin" in session:
-        return render_template("admin.html")
-    return redirect("/login")
 
 
 if __name__ == "__main__":
