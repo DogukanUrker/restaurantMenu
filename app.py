337c63b71fa1598ba6691584fef5289d56377ffe
Doğukan Ürker <dogukanurker@icloud.com>
2023-06-24 15:54:23 +0300

    removed unnecessary code

--- app.py
+++ app.py
@@ -3,24 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def loadData():
+@app.route("/")
+def index():
     foods = []
     drinks = []
     desserts = []
-    for i in open("products/food.txt", "r"):
+    for i in open("products/foods.txt", "r"):
         foods.append(i.strip("\n").split("//"))
-    for i in open("products/drink.txt", "r"):
+    for i in open("products/drinks.txt", "r"):
         drinks.append(i.strip("\n").split("//"))
-    for i in open("products/dessert.txt", "r"):
+    for i in open("products/desserts.txt", "r"):
         desserts.append(i.strip("\n").split("//"))
-    return foods, drinks, desserts
-
-
-@app.route("/")
-def index():
-    return render_template(
-        "index.html", foods=loadData()[0], drinks=loadData()[1], desserts=loadData()[2]
-    )
+    return render_template("index.html", foods=foods, drinks=drinks, desserts=desserts)
 
 
 if __name__ == "__main__":
